7be18cca43c3ee9da30aad502b2a561582dfa506
Sangntn <sangntn2101@gmail.com>
2023-07-25 16:00:39 +0700

    am_Sang_fix_bug_Mobile_UI_Design_Xml

--- app/src/am_sangntn/com/example/asian/Ex01RelativeLayout.java
+++ app/src/am_sangntn/com/example/asian/Ex01RelativeLayout.java
@@ -1,14 +1,68 @@
 package com.example.asian;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Patterns;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
+import java.util.regex.Pattern;
+
+@RequiresApi(api = Build.VERSION_CODES.O_MR1)
 public class Ex01RelativeLayout extends AppCompatActivity {
+
+    private EditText editTextEmail, editTextPassword;
+    private Button btnLogin;
+
+    public static final String PASSWORD_PATTERN = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_relativelayout);
+
+        editTextEmail = (EditText) findViewById(R.id.editTextUsername);
+        editTextPassword = (EditText) findViewById(R.id.editTextPassword);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkValidation();
+            }
+        });
+    }
+
+    public void checkValidation() {
+        if (!checkEmail(editTextEmail)) {
+            editTextEmail.setError("Username must be a Email and >=8 characters!");
+        }
+        if (!checkPassword(editTextPassword)) {
+            editTextPassword.setError("Password must contain a number, a Upper character, a special character and >= 8 characters!");
+        }
+    }
+
+    boolean checkLength(EditText editText) {
+        CharSequence str = editText.getText().toString();
+        return (str.length()>=8);
+    }
+
+    boolean checkEmail(EditText editText) {
+        CharSequence email = editText.getText().toString();
+        return  (email.length() > 8 && Patterns.EMAIL_ADDRESS.matcher(email).matches());
+    }
+
+    boolean checkPassword(EditText editText) {
+        CharSequence password = editText.getText().toString();
+        return (isValidPassword(editText.getText().toString()));
+    }
+
+    public static boolean isValidPassword(String s) {
+        Pattern password_pattern = Pattern.compile(PASSWORD_PATTERN);
+        return password_pattern.matcher(s).matches();
     }
 }
