15db93075db9a58f75948f71e8b1b3edf9e92988
Sangntn <sangntn2101@gmail.com>
2023-07-25 09:19:24 +0700

    am_Sang_new_feature_Mobile_UI_Design_Xml

---
+++ app/src/am_sangntn/com/example/asian/Ex03ConstraintLayout.java
@@ -0,0 +1,15 @@
+
+package com.example.asian;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ex03ConstraintLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_constraintlayout);
+    }
+}
