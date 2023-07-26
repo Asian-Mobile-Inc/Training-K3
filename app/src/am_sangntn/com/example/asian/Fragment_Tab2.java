b2f5fa8762569b29fc3b4be6dcaa05d11645d226
Sangntn <sangntn2101@gmail.com>
2023-07-26 14:47:32 +0700

    am_Sang_new_feature_Tablayout_Recyclerview_demo

--- app/src/am_sangntn/com/example/asian/Fragment_Tab2.java
+++ app/src/am_sangntn/com/example/asian/Fragment_Tab2.java
@@ -1,19 +1,19 @@
 package com.example.asian;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class Fragment_Tab1 extends Fragment {
+
+import androidx.fragment.app.Fragment;
+
+public class Fragment_Tab2 extends Fragment {
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab1, container, false);
+        return inflater.inflate(R.layout.fragment_tab2, container, false);
     }
 }
--- app/src/am_sangntn/com/example/asian/TabLayout_RecyclerView.java
+++ app/src/am_sangntn/com/example/asian/TabLayout_RecyclerView.java
@@ -1,2 +1,18 @@
-package com.example.asian;public class Tablayout {
+package com.example.asian;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class TabLayout_RecyclerView extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 }
