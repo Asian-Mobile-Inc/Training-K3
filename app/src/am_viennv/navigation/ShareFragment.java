6183f00aa78880911cd1ee451c24e29a88bb5041
viennguyen <viennv2002@gmail.com>
2023-07-28 14:40:30 +0700

    am_viennv new feature do with navigation/menu item and toolbar

---
+++ app/src/am_viennv/navigation/ShareFragment.java
@@ -0,0 +1,24 @@
+package navigation;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.asian.R;
+
+public class ShareFragment extends Fragment {
+
+    public ShareFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_share, container, false);
+    }
+
+}
